bf1c915f4ddf3cd275551231d0db070d020a841d
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-03-19 02:24:49 +0300

    добавление словаря в Template

--- my_jinia/lesson_1.py
+++ my_jinia/lesson_1.py
@@ -1,24 +1,12 @@
 from jinja2 import Template
 
-
 # {% %} спецификатор шаблона
 # {{ }} выражение для вставки конструкций Python в шаблон
 # {# #} блок комментариев
 # ## строковый комментарий
 
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
 
-    def get_name(self):
-        return self.name
-
-    def get_age(self):
-        return self.age
-
-
-person = Person('Юрий', 39)
-tm = Template('Привет, мне {{ p.get_age() }} лет и меня зовут {{ p.get_name() }}')
+person = {'name': 'Юрий', 'age': 39}
+tm = Template('Привет, мне {{ p.age }} лет и меня зовут {{ p.name }}')
 msg = tm.render(p=person)
 print(msg)
