81a54cab03b016a9472ab2f88bd82959ae7d67b3
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-03-19 02:12:55 +0300

    добавление конструкций python

--- my_jinia/lesson_1.py
+++ my_jinia/lesson_1.py
@@ -7,6 +7,6 @@ from jinja2 import Template
 
 name = 'Фёдор'
 age = 37
-tm = Template('Привет, мне {{ a }} лет и меня зовут {{ n }}')
+tm = Template('Привет, мне {{ a * 2 }} лет и меня зовут {{ n.upper() }}')
 msg = tm.render(n=name, a=age)
 print(msg)
