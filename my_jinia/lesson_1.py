55722a45fa8dc206aba95565bac71d877fc693ad
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-03-19 02:21:27 +0300

    добавление объекта класса в Template

--- my_jinia/lesson_1.py
+++ my_jinia/lesson_1.py
@@ -1,12 +1,24 @@
 from jinja2 import Template
 
+
 # {% %} спецификатор шаблона
 # {{ }} выражение для вставки конструкций Python в шаблон
 # {# #} блок комментариев
 # ## строковый комментарий
 
-name = 'Фёдор'
-age = 37
-tm = Template('Привет, мне {{ a * 2 }} лет и меня зовут {{ n.upper() }}')
-msg = tm.render(n=name, a=age)
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+
+person = Person('Юрий', 39)
+tm = Template('Привет, мне {{ p.get_age() }} лет и меня зовут {{ p.get_name() }}')
+msg = tm.render(p=person)
 print(msg)
