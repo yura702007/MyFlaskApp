9160e765e454dd6f123244e06960f30331dbea16
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-03-20 15:55:29 +0300

    init

---
+++ my_jinia/lesson_3.py
@@ -0,0 +1,8 @@
+from jinja2 import Template
+
+cars = [
+    {'model': 'Ауди', 'price': 23000},
+    {'model': 'Шкода', 'price': 17300},
+    {'model': 'Вольво', 'price': 44300},
+    {'model': 'Опель', 'price': 21300}
+]
