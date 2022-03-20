7f7206d2599a378988b6608bc7d919c994dd57b4
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-03-20 18:50:33 +0300

    make block for in macros

--- my_jinia/lesson_3.py
+++ my_jinia/lesson_3.py
@@ -1,15 +1,34 @@
 from jinja2 import Template
 
-html_tm = '''
-{%- macro input(name, value='', type='text', size=50) -%}
-    <input type="{{ type }}" name="{{ name }}" value="{{ value|e }}" size="{{ size }}">
-{%- endmacro -%}
+persons = [
+    {'name': 'Алексей', 'age': 18, 'weight': 78.5},
+    {'name': 'Николай', 'age': 28, 'weight': 82.3},
+    {'name': 'Иван', 'age': 33, 'weight': 94.0},
+]
 
-<p>{{ input('username') }}</p>
-<p>{{ input('email') }}</p>
-<p>{{ input('password') }}</p>
+html_tm = '''
+{%- macro list_users(list_of_users) -%}
+<ul>
+{%- for u in list_of_users %}
+    <li>{{ u.name }}</li>
+{%- endfor %}
+</ul>
+{% endmacro -%}
+
+{{list_users(users)}}
 '''
 
 tm = Template(html_tm)
-msg = tm.render()
+msg = tm.render(users=persons)
 print(msg)
+'''
+{% macro list_users(list_of_user) -%}
+<ul>
+{% for u in users -%}
+    <li>{{u.name}} 
+{%- endfor %}
+</ul>
+{%- endmacro %}
+
+{{list_users(users)}}
+'''
