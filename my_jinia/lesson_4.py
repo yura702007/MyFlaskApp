c2882889e6e983895dd24417cc5666d0c5da3e1b
yura702007 <82802568+yura702007@users.noreply.github.com>
2022-03-22 15:21:22 +0300

    use FunctionLoader

--- my_jinia/lesson_4.py
+++ my_jinia/lesson_4.py
@@ -1,4 +1,12 @@
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, FunctionLoader
+
+
+def load_tmp(path):
+    if path == 'index':
+        return '''Имя{{u.name}}, возраст {{u.age}}, вес {{u.weight}}'''
+    else:
+        return '''Данные: {{u}}'''
+
 
 persons = [
     {'name': 'Алексей', 'age': 18, 'weight': 78.5},
@@ -6,9 +14,10 @@ persons = [
     {'name': 'Иван', 'age': 33, 'weight': 94.0}
 ]
 
-file_loader = FileSystemLoader('templates')  # загрузка директории шаблонов
+# file_loader = FileSystemLoader('templates')  # загрузка директории шаблонов
+file_loader = FunctionLoader(load_tmp)  # загрузка шаблона из функции
 env = Environment(loader=file_loader)  # создание окружения
 
-tm = env.get_template('main.html')  # загрузка файла шаблона
-msg = tm.render(users=persons)
+tm = env.get_template('index')  # загрузка файла шаблона
+msg = tm.render(u=persons[0])
 print(msg)
