51ad1392192fe88f3c17e56444fbb61658800217
yura702007 <82802568+yura702007@users.noreply.github.com>
2022-03-22 16:46:48 +0300

    use include command

--- my_jinia/lesson_5.py
+++ my_jinia/lesson_5.py
@@ -2,7 +2,7 @@ from jinja2 import Environment, FileSystemLoader
 
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
-tm = env.get_template('page.html')
+tm = env.get_template('head.html')
 
 msg = tm.render()
 print(msg)
