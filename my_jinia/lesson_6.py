375591ce339c31f0efb114226d35ba8f676a7149
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-03-24 02:25:24 +0300

    создание вложенного блока

--- my_jinia/lesson_6.py
+++ my_jinia/lesson_6.py
@@ -1,8 +1,10 @@
 from jinja2 import FileSystemLoader, Environment
 
+items = ['физика', 'химия', 'математика', 'информатика']
+
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
 
 template = env.get_template('about.html')
-output = template.render()
+output = template.render(list_table=items)
 print(output)
