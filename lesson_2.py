f41486bb35d9126e14559061714011561ce05c6d
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-03-26 13:57:57 +0300

    add parameter 'title' in render_template

--- lesson_2.py
+++ lesson_2.py
@@ -3,14 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Про Flask')
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html',  title='О сайте')
 
 
 if __name__ == '__main__':
