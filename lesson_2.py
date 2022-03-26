8550d385e2a775b6f9450ce6090f05ec1d5d18a7
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-03-26 14:05:30 +0300

    add menu in templates

--- lesson_2.py
+++ lesson_2.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template
 
+MENU = ['Установка', 'Первое приложение', 'Обратная связь']
+
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Про Flask')
+    return render_template('index.html', title='Про Flask', menu=MENU)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html',  title='О сайте')
+    return render_template('about.html',  title='О сайте', menu=MENU)
 
 
 if __name__ == '__main__':
