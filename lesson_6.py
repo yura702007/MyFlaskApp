e174120c15fbda8dd39ce0119cc837635fcb4f04
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-03-27 17:15:50 +0300

    import flash function

--- lesson_6.py
+++ lesson_6.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 
 MENU = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
