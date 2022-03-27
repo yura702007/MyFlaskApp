5b076acb469b6dcecabb15fd4888fdfbe9428066
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-03-27 15:59:59 +0300

    add getting post-request

--- lesson_5.py
+++ lesson_5.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 MENU = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
@@ -18,8 +18,10 @@ def about():
     return render_template('about.html',  title='О сайте', menu=MENU)
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['POST', 'GET'])
 def contact():
+    if request.method == 'POST':
+        print(request.form)
     return render_template('contact.html',  title='Обратная связь', menu=MENU)
 
 
