4eed066dfab70e90012729a9e6dafe64f83c12c7
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-03-27 11:44:05 +0300

    init

---
+++ lesson_4.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+MENU = ['Установка', 'Первое приложение', 'Обратная связь']
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', menu=MENU)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html',  title='О сайте', menu=MENU)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
