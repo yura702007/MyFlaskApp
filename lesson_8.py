42dc4448810cbee1dc4224652d3a56d38c345db0
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-03-29 02:35:08 +0300

    add data config

--- lesson_8.py
+++ lesson_8.py
@@ -1,3 +1,13 @@
 import sqlite3
 import os
 from flask import Flask, render_template, request
+
+# конфигурация
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
+USERNAME = 'admin'
+PASSWORD = '123'
+
+if __name__ == '__main__':
+    pass
