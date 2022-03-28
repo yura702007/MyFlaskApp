3c2fb7c036f2a79e2ff77321f73ec7fc20bf893e
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-03-29 02:51:37 +0300

    add application

--- lesson_8.py
+++ lesson_8.py
@@ -2,12 +2,20 @@ import sqlite3
 import os
 from flask import Flask, render_template, request
 
-# конфигурация
+# config
 DATABASE = '/tmp/flsite.db'
 DEBUG = True
 SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
 USERNAME = 'admin'
 PASSWORD = '123'
+
+# create web_application
+app = Flask(__name__)
+# load config from this module
+app.config.from_object(__name__)
+
+# change value DATABASE placing database in current application directory
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
 if __name__ == '__main__':
     pass
