8cd9549588afd39bf6ce554c2952194998e9ece5
yura702007 <82802568+yura702007@users.noreply.github.com>
2022-04-06 12:46:58 +0300

    init

---
+++ lesson_10.py
@@ -0,0 +1,90 @@
+import sqlite3
+import os
+from flask import Flask, render_template, request, g, flash, abort
+from f_data_base import FDataBase
+
+# config
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
+USERNAME = 'admin'
+PASSWORD = '123'
+
+# create web_application
+app = Flask(__name__)
+# load config from this module
+app.config.from_object(__name__)
+
+# change value DATABASE placing database in current application directory
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+
+def connect_db():
+    """connect with database"""
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    """
+    create tables in database
+    export in console and run
+    """
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    """Database connection if not already established"""
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Close the connection to the database if it was established"""
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+@app.route('/')
+def index():
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
+
+
+@app.route('/add_post', methods=['POST', 'GET'])
+def add_post():
+    db = get_db()
+    dbase = FDataBase(db)
+    if request.method == 'POST':
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.add_post(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+    return render_template('add_post.html', menu=dbase.getMenu(), title='Добавление статьи')
+
+
+@app.route("/post/<int:id_post>")
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG)
--- sq_db.sql
+++ sq_db.sql
@@ -8,5 +8,6 @@ create table if not exists posts (
 id integer primary key autoincrement,
 title text not null,
 text text not null,
+url text not null,
 time integer not null
 );
