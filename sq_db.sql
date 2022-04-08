971e2828920f229faf81bda1f0276fd5f68410a7
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-04-08 03:21:08 +0300

    create table 'users' in database

--- sq_db.sql
+++ sq_db.sql
@@ -11,3 +11,11 @@ text text not null,
 url text not null,
 time integer not null
 );
+
+CREATE TABLE IF NOT EXISTS users (
+id integer PRIMARY KEY AUTOINCREMENT,
+name text NOT NULL,
+email text NOT NULL,
+psw text NOT NULL,
+time integer NOT NULL
+);
