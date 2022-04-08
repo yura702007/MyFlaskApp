5c7849638928e344eec7bc777ff875cab9b73e40
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-04-08 04:20:30 +0300

    add addUser method

--- f_data_base.py
+++ f_data_base.py
@@ -61,3 +61,19 @@ class FDataBase:
             print("Ошибка получения статьи из БД " + str(e))
 
         return (False, False)
+
+    def addUser(self, name, email, hash_psw):
+        try:
+            self.__cur.execute(f"SELECT COUNT() as `count` FROM users WHERE email LIKE '{email}'")
+            res = self.__cur.fetchone()
+            if res['count'] > 0:
+                print('Пользователь с таким email уже существует')
+                return False
+            tm = math.floor(time.time())
+            self.__cur.execute("INSERT INTO users VALUES(NULL, ?, ?, ?, ?)", (name, email, hash_psw, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print(f'Ошибка добавления пользователя в БД {e}')
+            return False
+        return True
+
