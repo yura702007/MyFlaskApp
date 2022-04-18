a6f1180d2b3a9398ee432558238c72d5e66968d0
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-04-18 04:41:23 +0300

    update function register and data base file

--- lesson_19.py
+++ lesson_19.py
@@ -5,7 +5,7 @@ from f_data_base import FDataBase
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_user, login_required, current_user, logout_user
 from user_login import UserLogin
-from forms import LoginForm
+from forms import LoginForm, RegisterForm
 
 # config
 DATABASE = '/tmp/flsite.db'
@@ -128,22 +128,16 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        session.pop('_flashes', None)
-        if len(request.form['name']) > 4 and len(request.form['email']) > 4 and len(request.form['psw']) > 4 \
-                and request.form['psw'] == request.form['psw2']:
-            psw = request.form['psw']
-            _hash = generate_password_hash(psw)
-            res = dbase.addUser(request.form['name'], request.form['email'], _hash)
-            if res:
-                flash("Вы успешно зарегистрированы", "success")
-                return redirect(url_for('login'))
-            else:
-                flash("Ошибка при добавлении в БД", "error")
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hash = generate_password_hash(request.form['psw'])
+        res = dbase.addUser(form.name.data, form.email.data, hash)
+        if res:
+            flash('Вы успешно зарегистрированы', 'success')
+            return redirect(url_for('login'))
         else:
-            flash("Неверно заполнены поля", "error")
-
-    return render_template('register.html', menu=dbase.getMenu(), title='Регистрация')
+            flash('Ошибка при добавлении в БД', 'error')
+    return render_template('register.html', menu=dbase.getMenu(), title='Регистрация', form=form)
 
 
 @app.route('/profile')
