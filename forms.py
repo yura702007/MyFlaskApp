eaa935c2fc2b87ed55b92157c509d49d0101c10b
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-04-18 04:17:59 +0300

    add class RegisterForm

--- forms.py
+++ forms.py
@@ -1,12 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, PasswordField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email: ', validators=[Email(message='Некорректный email')])
     psw = PasswordField('Пароль: ', validators=[
-        DataRequired(), Length(min=4, max=100, message='Пароль должен быть от 4 до 100 символов')]
+        DataRequired(), Length(min=4, max=100, message='Пароль должен быть от 4 до 100 символов')
+    ]
                         )
     remember = BooleanField('Запомнить', default=False)
     submit = SubmitField('Войти')
+
+
+class RegisterForm(FlaskForm):
+    name = StringField('Имя: ', validators=[Length(min=4, max=100, message='Имя должно быть от 4 до 100 символов')])
+    email = StringField('Email: ', validators=[Email(message='Некорректный email')])
+    psw = PasswordField('Пароль: ', validators=[
+        DataRequired(),
+        Length(min=4, max=100, message='Пароль должен быть от 4 до 100 символов')
+    ]
+                        )
+    psw2 = PasswordField('Повтор пароля: ', validators=[DataRequired(), EqualTo('psw', message='Пароли не совпадают')])
+    submit = SubmitField('Регистрация')
