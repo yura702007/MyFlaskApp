7f37d66694697d9df287a00ac0494ec9ac4c98df
yury702007dev <90982097+yury702007dev@users.noreply.github.com>
2022-04-18 03:34:22 +0300

    add messages in attributes email and psw

--- forms.py
+++ forms.py
@@ -4,7 +4,9 @@ from wtforms.validators import DataRequired, Email, Length
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email: ', validators=[Email()])
-    psw = PasswordField('Пароль: ', validators=(DataRequired(), Length(min=4, max=100)))
+    email = StringField('Email: ', validators=[Email(message='Некорректный email')])
+    psw = PasswordField('Пароль: ', validators=[
+        DataRequired(), Length(min=4, max=100, message='Пароль должен быть от 4 до 100 символов')]
+                        )
     remember = BooleanField('Запомнить', default=False)
     submit = SubmitField('Войти')
