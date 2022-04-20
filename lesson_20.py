8a5dc408483c37250340ed56fcc82900dc865eb6
yura702007 <82802568+yura702007@users.noreply.github.com>
2022-04-20 11:55:01 +0300

    init

---
+++ lesson_20.py
@@ -0,0 +1,189 @@
+import sqlite3
+import os
+from flask import Flask, render_template, request, g, flash, abort, session, redirect, url_for, make_response
+from f_data_base import FDataBase
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
+from user_login import UserLogin
+from forms import LoginForm, RegisterForm
+
+# config
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
+USERNAME = 'admin'
+PASSWORD = '123'
+MAX_CONTENT_LENGTH = 1024 * 1024
+
+# create web_application
+app = Flask(__name__)
+# load config from this module
+app.config.from_object(__name__)
+
+# change value DATABASE placing database in current application directory
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+dbase = None
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Авторизуйтесь для доступа к закрытым страницам'
+login_manager.login_message_category = 'success'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    print('load user')
+    return UserLogin().from_db(user_id, dbase)
+
+
+@app.before_request
+def before_request():
+    """
+    Establishing a database connection before executing a query
+    """
+    global dbase
+    db = get_db()
+    dbase = FDataBase(db)
+
+
+def connect_db():
+    """connect with database"""
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    """
+    create tables in database
+    export in console and run
+    """
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    """Database connection if not already established"""
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Close the connection to the database if it was established"""
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
+
+
+@app.route('/add_post', methods=['POST', 'GET'])
+def add_post():
+    if request.method == 'POST':
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.add_post(request.form['name'], request.form['post'], request.form['url'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+    return render_template('add_post.html', menu=dbase.getMenu(), title='Добавление статьи')
+
+
+@app.route("/post/<alias>")
+@login_required
+def showPost(alias):
+    title, post = dbase.getPost(alias)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = dbase.getUserByEmail(form.email.data)
+        if user and check_password_hash(user['psw'], form.psw.data):
+            userlogin = UserLogin().create(user)
+            rm = form.remember.data
+            login_user(userlogin, remember=rm)
+            return redirect(request.args.get('next') or url_for('profile'))
+        flash('Неверная пара логин/пароль', 'error')
+    form.psw.flags.__delattr__('minlength')
+    form.psw.flags.__delattr__('maxlength')
+    return render_template('login.html', menu=dbase.getMenu(), title='Авторизация', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hash = generate_password_hash(request.form['psw'])
+        res = dbase.addUser(form.name.data, form.email.data, hash)
+        if res:
+            flash('Вы успешно зарегистрированы', 'success')
+            return redirect(url_for('login'))
+        else:
+            flash('Ошибка при добавлении в БД', 'error')
+    return render_template('register.html', menu=dbase.getMenu(), title='Регистрация', form=form)
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', menu=dbase.getMenu(), title='Профиль')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Вы вышли из аккаунта', 'success')
+    return redirect(url_for('login'))
+
+
+@app.route('/userava')
+@login_required
+def userava():
+    img = current_user.getAvatar(app)
+    if not img:
+        return ''
+    h = make_response(img)
+    h.headers['Content-Type'] = 'image/png'
+    return h
+
+
+@app.route('/upload', methods=['POST', 'GET'])
+@login_required
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and current_user.verifyExt(file.filename):
+            try:
+                img = file.read()
+                res = dbase.updateUserAvatar(img, current_user.get_id())
+                if not res:
+                    flash("Ошибка обновления аватара", "error")
+                    return redirect(url_for('profile'))
+                flash("Аватар обновлен", "success")
+            except FileNotFoundError as e:
+                flash("Ошибка чтения файла", "error")
+        else:
+            flash("Ошибка обновления аватара", "error")
+        return redirect(url_for('profile'))
+
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG, port=4242)
