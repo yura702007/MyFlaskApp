1fd161de606049d777f6403d58c8c8f879401034
yura702007 <82802568+yura702007@users.noreply.github.com>
2022-04-20 18:32:41 +0300

    create instance SQLAlchemy as db

--- my_sql_achemy/flask_app.py
+++ my_sql_achemy/flask_app.py
@@ -3,5 +3,8 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+db = SQLAlchemy(app)
+
 if __name__ == '__main__':
     app.run(debug=True, port=4242)
