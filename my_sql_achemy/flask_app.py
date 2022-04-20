c4f9b22d78ea27fee7c02bb0d1fd1ce4d3dc8ed3
yura702007 <82802568+yura702007@users.noreply.github.com>
2022-04-20 18:55:24 +0300

    add class Profiles

--- my_sql_achemy/flask_app.py
+++ my_sql_achemy/flask_app.py
@@ -11,8 +11,22 @@ db = SQLAlchemy(app)
 class Users(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     user_email = db.Column(db.String(50), unique=True)
-    psw = db.Column(db.String(500), unique=True)
+    psw = db.Column(db.String(500), nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<users {self.user_id}>"
+
+
+class Profiles(db.Model):
+    col_id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(50), nullable=False)
+    age = db.Column(db.Integer)
+    city = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    def __repr__(self):
+        return f'<profiles {self.col_id}>'
 
 
 if __name__ == '__main__':
