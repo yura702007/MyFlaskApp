fc18a67abd2137a833c7feb90dfddc00ec0a3f70
yura702007 <82802568+yura702007@users.noreply.github.com>
2022-04-20 13:30:44 +0300

    add functions 'login' and 'login_admin'

--- admin/admin.py
+++ admin/admin.py
@@ -1,7 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, request, redirect, url_for, flash, render_template, session
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
+
+
+def login_admin():
+    session['admin_logget'] = 1
+
 
 @admin.route('/')
 def index():
     return '<h1>Администратор</h1>'
+
+
+@admin.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form['user'] == 'admin' and request.form['psw'] == '12345':
+            login_admin()
+            return redirect(url_for('.index'))
+        else:
+            flash('Неверная пара логин/пароль', 'error')
+    return render_template('admin/login.html', title='Админ-панель')
