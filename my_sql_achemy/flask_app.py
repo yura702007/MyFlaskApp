2260f7c922081c4852a6650664904a69544b4cbd
yura702007 <82802568+yura702007@users.noreply.github.com>
2022-04-20 18:27:56 +0300

    init

---
+++ my_sql_achemy/flask_app.py
@@ -0,0 +1,7 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+if __name__ == '__main__':
+    app.run(debug=True, port=4242)
