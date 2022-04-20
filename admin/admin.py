3f73e0565e0274ef784a2ffa7863180255311c3b
yura702007 <82802568+yura702007@users.noreply.github.com>
2022-04-20 12:58:31 +0300

    add function 'index'

--- admin/admin.py
+++ admin/admin.py
@@ -1,3 +1,7 @@
 from flask import Blueprint
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
+
+@admin.route('/')
+def index():
+    return '<h1>Администратор</h1>'
